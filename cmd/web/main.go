f1c131b6d209f08915bca6ad56845bd0f2dc92f1
Ayan Sarkar <ayansarkar367@gmail.com>
2021-04-27 21:23:10 +0530

    initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/ayans101/go-bookings-app/pkg/config"
+	"github.com/ayans101/go-bookings-app/pkg/handlers"
+	"github.com/ayans101/go-bookings-app/pkg/render"
+)
+
+const portNumber = ":4000"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+func main() {
+
+	//	change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	render.NewTemplates(&app)
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	fmt.Println("Starting application on port", portNumber)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+
+}
+
+//	go run cmd/web/*.go
